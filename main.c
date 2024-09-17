c7853b2b49c3fec30254b9441024d7ce2badd259
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-17 23:32:16 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Date {
+    int jour, mois, annee;
+} Date;
+
+typedef struct Client {
+    char num_compte[8];
+    char nom[20];
+    char operation;
+    float anc_solde, nouv_solde;
+    Date date;
+} Client;
+
+int main(){
+
+    printf("\n");
+    return 0;
+}
+
+void ouvre_fichier(char * nom_fichier){
+    FILE * file;
+}
