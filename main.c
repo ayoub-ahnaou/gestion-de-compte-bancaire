9657245e11a93c7bd1617a86a3a1def2b1dae44a
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-19 15:51:21 +0100

    ajout de fonctions  retrait et virment

--- main.c
+++ main.c
@@ -16,16 +16,15 @@ typedef struct Date {
 typedef struct Client {
     char num_compte[9];
     char username[30];
-    float anc_solde;
-    float nouv_solde;
+    float solde;
     Date date;
     char operation;
 } Client;
 
 Client clients[10] = {
-    {"12345673", "alice", 0, 1200.75, {12, 9, 2024}},
-    {"23456478", "bob", 0, 450.00, {10, 9, 2024}},
-    {"34567869", "charlie", 0, 2300.00, {9, 9, 2024}}
+    {"12345673", "alice", 1200.75, {12, 9, 2024}},
+    {"23456478", "bob", 450.00, {10, 9, 2024}},
+    {"34567869", "charlie", 2300.00, {9, 9, 2024}}
 } ;
 int size = 3;
 
@@ -38,6 +37,9 @@ void afficher_client();
 void rechercher_par_num();
 void rechercher_par_nom();
 void lister_clients();
+void effectuer_operation();
+void effectuer_retrait(int position);
+void effectuer_virement(int position);
 
 int main(){
     menu();
@@ -65,7 +67,7 @@ void menu(){
             case 1: ajouter_client(); break;
             case 2: afficher_client(); break;
             case 3: lister_clients(); break;
-            case 4: break;
+            case 4: effectuer_operation(); break;
             case 5: exit(0); break;
             default:
                 system("clear");
@@ -104,7 +106,7 @@ void ajouter_client(){
     scanf(" %[^\n]s", clients[size].num_compte);
     printf("nom et prenom: ");
     scanf(" %[^\n]s", clients[size].username);
-    printf("votre solde: "); scanf("%f", &clients[size].nouv_solde);
+    printf("votre solde: "); scanf("%f", &clients[size].solde);
     printf("date de naissance (JJ/MM/AAAA): "); scanf("%d/%d/%d", &clients[size].date.jour, &clients[size].date.mois, &clients[size].date.annee);
     printf(COLOR_GREEN "\nnouveau client a ete ajoutee avec le numero '%s'..\n" COLOR_RESET, clients[size].num_compte);
     
@@ -149,7 +151,7 @@ void rechercher_par_num(){
     for(int i=0; i<size; i++){
         if(strcmp(clients[i].num_compte, num) == 0){
             printf(COLOR_VIOLET "num de compte    username       date naissance   solde actuel\n" COLOR_RESET);
-            printf("%-16s %-14s %-2d/%-2d/%-10d %.2f \n", clients[i].num_compte, clients[i].username, clients[i].date.jour, clients[i].date.mois, clients[i].date.annee, clients[i].nouv_solde);
+            printf("%-16s %-14s %-2d/%-2d/%-10d %.2f \n", clients[i].num_compte, clients[i].username, clients[i].date.jour, clients[i].date.mois, clients[i].date.annee, clients[i].solde);
             return;
         }
     }
@@ -169,7 +171,7 @@ void rechercher_par_nom(){
     for(int i=0; i<size; i++){
         if(strcmp(clients[i].username, nom) == 0){
             printf(COLOR_VIOLET "num de compte    username       date naissance   solde actuel\n" COLOR_RESET);
-            printf("%-16s %-14s %-2d/%-2d/%-10d %.2f \n", clients[i].num_compte, clients[i].username, clients[i].date.jour, clients[i].date.mois, clients[i].date.annee, clients[i].nouv_solde);
+            printf("%-16s %-14s %-2d/%-2d/%-10d %.2f \n", clients[i].num_compte, clients[i].username, clients[i].date.jour, clients[i].date.mois, clients[i].date.annee, clients[i].solde);
             return;
         }
     }
@@ -187,7 +189,74 @@ void lister_clients(){
     }
     printf(COLOR_VIOLET "num de compte    username       date naissance   solde actuel\n" COLOR_RESET);
     for(int i=0; i<size; i++){
-        printf("%-16s %-14s %-2d/%-2d/%-10d %.2f \n", clients[i].num_compte, clients[i].username, clients[i].date.jour, clients[i].date.mois, clients[i].date.annee, clients[i].nouv_solde);
+        printf("%-16s %-14s %-2d/%-2d/%-10d %.2f \n", clients[i].num_compte, clients[i].username, clients[i].date.jour, clients[i].date.mois, clients[i].date.annee, clients[i].solde);
     }
 }
 
+void effectuer_operation(){
+    system("clear");
+    printf(COLOR_YELLOW "-----------------------------------------------------------------------\n");
+    printf("                        effectuer une operation                        \n");
+    printf("-----------------------------------------------------------------------\n" COLOR_RESET);
+    char nom[30]; int position = -1;
+    printf("entrer votre nom de compte: ");
+    scanf(" %[^\n]s", nom);
+    printf("-----------------------------------------------------------------------\n");
+    for(int i=0; i<size; i++){
+        if(strcmp(clients[i].username, nom) == 0){
+            position = i;
+        }
+    }
+    if(position == -1){
+        printf(COLOR_RED "compte ne pas trouvee \n" COLOR_RESET);
+        return;
+    }
+    else {
+        int choice;
+        do{
+            puts("1- effectuer un retrait");
+            puts("2- effectuer un virement");
+            puts("3- retour au menu principale");
+            printf("-----------------------------------------------------------------------\n");
+            printf("choisit une operation: "); scanf("%d", &choice);
+            switch(choice){
+                case 1: effectuer_retrait(position); break;
+                case 2: effectuer_virement(position); break;
+                case 3: return; break;
+                default:
+                    system("clear");
+                    printf(COLOR_RED "veuillez choisit un choix valide dans le menu.\n" COLOR_RESET);
+                    break;
+            }
+        }
+        while(choice != 3);
+    }
+}
+
+void effectuer_retrait(int position){
+    system("clear");
+    float amount;
+    printf("entrer votre montant pour votre retrait: ");
+    scanf("%f", &amount);
+    if(amount > clients[position].solde){
+        printf(COLOR_RED "vous n'avez pas cet amount dans votre compte\n" COLOR_RESET);
+        return;
+    }
+    else {
+        clients[position].solde = clients[position].solde - amount;
+        printf(COLOR_GREEN "vous avez effectue un retrait du %.2f DH avec succes\n" COLOR_RESET, amount);
+    }
+}
+
+void effectuer_virement(int position){
+    system("clear");
+    float amount;
+    printf("entrer votre montant pour votre virement: ");
+    scanf("%f", &amount);
+    if(amount < 100){
+        printf(COLOR_RED "vous pouvez verser au minum 100 DH dans votre compte\n" COLOR_RESET);
+        return;
+    }
+    clients[position].solde = clients[position].solde + amount;
+    printf(COLOR_GREEN "vous avez effectue un retrait du %.2f DH avec succes\n" COLOR_RESET, amount);
+}
